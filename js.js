3ef2607ddc0311d9ef00683f691928ebd6e88402
NTH2112 <nhamtoanhuu1@gmail.com>
2024-09-30 10:54:16 +0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,5 @@
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Cảm ơn bạn đã gửi tin nhắn!");
+    this.reset(); // Reset form fields
+});
